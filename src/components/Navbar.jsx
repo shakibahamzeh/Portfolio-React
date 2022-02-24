042ccb809128ca16b0e5a99ae30d1ab0b3af6f72
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2022-02-24 10:51:21 +0330

    responsive site

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React,{useContext} from 'react';
+import React,{useContext , useState,useEffect} from 'react';
 import "./navbar.css";
 import { Link } from 'react-router-dom';
 import { ThemeContext } from '../context/ThemeContextProvider';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () =>{
     const Theme = useContext(ThemeContext);
@@ -12,23 +13,45 @@ const Navbar = () =>{
     else
       Theme.dispatch({ type: "DARK_MODE" });
   };
+   const [toggleMenu, setToggleMenu] = useState(false);
+   const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const MenuHandler =()=>{
+     setToggleMenu(!toggleMenu)
+  }
+  useEffect(() => {
 
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth);
+     return () => {
+        window.removeEventListener('resize', changeWidth)
+    }
+
+  }, [])
   return (
     <header className={`header ${darkMode ? "header-dark" : "header-light"}`}>
         <div className="logo">
             <h2>PORTFOLIO</h2>
         </div>
         <nav>
+           {(toggleMenu || screenWidth >768) && (
             <ul className="menu">
                 <li><Link to='/aboutUs'>About</Link></li>
                 <li><Link to='/works'>Works</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
+           )}
+            
         </nav>
+        <div className='menuIconWrapper'>
+              <MenuIcon className='menuIcon' onClick={MenuHandler}/>
+            </div>
         <div className="darkModeBtnWrapper">
             
 	        <input type="checkbox" id="toggle" className={`darkBtn ${darkMode ? "darkBtn-dark" : "darkBtn-light"}`} onClick={DarkModeHandler}/>
-	        <label for="toggle"></label>
+	        <label htmlFor="toggle"></label>
 
         </div>
     </header>
